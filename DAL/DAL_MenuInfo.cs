836ab5563d85185109104e8056c2cc4ebf920f8f
KelvanSun <kelvan.sun@gmail.com>
2014-07-25 15:09:56 +0800

    Add DAL_MenuInfo.cs file

---
+++ DAL/DAL_MenuInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+
+using Mondiland.Entity;
+using Mondiland.IDal;
+using Mondiland.Global;
+
+namespace Mondiland.DAL
+{
+    public class DAL_MenuInfo:BaseDal<Table_MenuInfo_Entity>
+    {
+
+    }
+}
