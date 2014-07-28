be35e0799182f1df737e5bba089e4f98790c7c12
KelvanSun <kelvan.sun@gmail.com>
2014-07-28 16:32:06 +0800

    提交

---
+++ UI/LoginForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Mondiland.UI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_cancel = new System.Windows.Forms.Button();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.cbx_username = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_pwd = new System.Windows.Forms.TextBox();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(149)))), ((int)(((byte)(193)))));
+            this.panel.Controls.Add(this.label1);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(415, 81);
+            this.panel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(73, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "宁波麦迪茉莱登商品管理系统";
+            // 
+            // bt_cancel
+            // 
+            this.bt_cancel.Location = new System.Drawing.Point(291, 178);
+            this.bt_cancel.Name = "bt_cancel";
+            this.bt_cancel.Size = new System.Drawing.Size(75, 23);
+            this.bt_cancel.TabIndex = 11;
+            this.bt_cancel.Text = "取消";
+            this.bt_cancel.UseVisualStyleBackColor = true;
+            this.bt_cancel.Click += new System.EventHandler(this.bt_cancel_Click);
+            // 
+            // bt_login
+            // 
+            this.bt_login.Location = new System.Drawing.Point(200, 178);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(75, 23);
+            this.bt_login.TabIndex = 10;
+            this.bt_login.Text = "登陆";
+            this.bt_login.UseVisualStyleBackColor = true;
+            this.bt_login.Click += new System.EventHandler(this.bt_login_Click);
+            // 
+            // cbx_username
+            // 
+            this.cbx_username.BackColor = System.Drawing.SystemColors.Window;
+            this.cbx_username.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_username.FormattingEnabled = true;
+            this.cbx_username.Location = new System.Drawing.Point(98, 95);
+            this.cbx_username.Name = "cbx_username";
+            this.cbx_username.Size = new System.Drawing.Size(221, 20);
+            this.cbx_username.TabIndex = 13;
+            this.cbx_username.SelectedIndexChanged += new System.EventHandler(this.cbx_username_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 19);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "密  码";
+            this.label4.UseCompatibleTextRendering = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "用户名";
+            this.label3.UseCompatibleTextRendering = true;
+            // 
+            // tb_pwd
+            // 
+            this.tb_pwd.Location = new System.Drawing.Point(98, 129);
+            this.tb_pwd.Name = "tb_pwd";
+            this.tb_pwd.PasswordChar = '*';
+            this.tb_pwd.Size = new System.Drawing.Size(221, 21);
+            this.tb_pwd.TabIndex = 0;
+            this.tb_pwd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_pwd_KeyDown);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 215);
+            this.Controls.Add(this.tb_pwd);
+            this.Controls.Add(this.bt_cancel);
+            this.Controls.Add(this.bt_login);
+            this.Controls.Add(this.cbx_username);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "系统登陆...";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_cancel;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.ComboBox cbx_username;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_pwd;
+    }
+}
---
+++ UI/LoginForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Mondiland.BLL;
+using Mondiland.Global;
+
+
+namespace Mondiland.UI
+{
+    public partial class LoginForm : Form
+    {
+        private MainForm m_mainForm = null;
+        public LoginForm(MainForm mainForm)
+        {
+            InitializeComponent();
+
+            m_mainForm = mainForm;
+        }
+
+        private void bt_cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bt_login_Click(object sender, EventArgs e)
+        {
+            if (tb_pwd.Text.Trim() == string.Empty)
+            {
+                MessageUtil.ShowTips("请输入密码!");
+                tb_pwd.Focus();
+
+                return;
+            }
+
+            if (!BLLFactory<BLLLogin>.Instance.Authentication(cbx_username.Text, tb_pwd.Text))
+            {
+                MessageUtil.ShowWarning("用户验证失败");
+                tb_pwd.Focus();
+                return;
+            }
+
+            m_mainForm.SetUserId(BLLFactory<BLLLogin>.Instance.GetUserID(cbx_username.Text));
+            this.Close();
+
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            List<string> list = BLLFactory<BLLLogin>.Instance.GetUserList();
+
+            IEnumerator<string> ator = list.GetEnumerator();
+
+            while (ator.MoveNext())
+            {
+                this.cbx_username.Items.Add(ator.Current.ToString());
+            }
+
+            this.cbx_username.SelectedIndex = 0;
+           
+        }
+
+        private void tb_pwd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) bt_login_Click(sender, e);
+        }
+
+        private void cbx_username_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tb_pwd.Text = string.Empty;
+            tb_pwd.Focus();
+    
+        }
+
+    }
+}
