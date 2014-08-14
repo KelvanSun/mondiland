3dbc6631ed83fcd0ce0d5abaf09d9bac42f5d377
Kelvan Sun <kelvan.sun@gmail.com>
2014-08-14 10:15:44 +0800

    提交

---
+++ Object/BaseProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mondiland.Object
+{
+    /// <summary>
+    /// 所有产品的基类
+    /// </summary>
+    public abstract class BaseProduct
+    {
+        /// <summary>
+        /// 以ID号填充数据
+        /// </summary>
+        /// <param name="id">ID号</param>
+        public abstract void ReadByPrimaryKey(int id);
+        /// <summary>
+        /// 以货号填充数据
+        /// </summary>
+        /// <param name="huo_hao">货号</param>
+        public abstract void ReadByHuoHao(string huo_hao);
+    }
+}
---
+++ Object/ShirtProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mondiland.Object
+{
+    public class ShirtProduct:BaseProduct
+    {
+        public override void ReadByPrimaryKey(int id)
+        {
+
+        }
+
+        public override void ReadByHuoHao(string huo_hao)
+        {
+
+        }
+    }
+}
