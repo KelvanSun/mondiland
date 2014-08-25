45819d891ed866c67a49d678b888b7f29ccfb12f
Kelvan Sun <kelvan.sun@gmail.com>
2014-08-25 10:36:42 +0800

    添加了类

---
+++ BLL/BLLSupplierInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.ComponentModel;
+
+using Mondiland.Entity;
+using Mondiland.IDal;
+using Mondiland.BLLEntity;
+using Mondiland.Global;
+
+namespace Mondiland.BLL
+{
+    public class BLLSupplierInfo:BaseBLL
+    {
+        private ITable_SupplierM SupplierM_Dal = null;
+        private ITable_SupplierD SupplierD_Dal = null;
+        public BLLSupplierInfo()
+        {
+            SupplierM_Dal = Reflect<ITable_SupplierM>.Create("DAL_SupplierM", "Mondiland.DAL");
+            SupplierD_Dal = Reflect<ITable_SupplierD>.Create("DAL_SupplierD", "Mondiland.DAL");
+        }
+    }
+}
---
+++ UI/SupplierObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration;
+
+using Mondiland.BLL;
+using Mondiland.BLLEntity;
+
+namespace Mondiland.UI
+{
+    public class SupplierObject
+    {
+    }
+}
