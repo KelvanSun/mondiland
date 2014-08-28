5f06772031047a7259834277a88dafe6e7f864e4
Kelvan Sun <kelvan.sun@gmail.com>
2014-08-28 10:32:21 +0800

    添加新object

---
+++ Object/PermissionObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mondiland.BLL;
+using Mondiland.BLLEntity;
+
+namespace Mondiland.Obj
+{
+    public class PermissionObject
+    {
+
+    }
+}
